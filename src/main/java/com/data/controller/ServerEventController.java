b70c5825e8944b257f03527ed63e831463b70965
Mayur T <mayurthube@gmail.com>
2025-07-04 00:14:40 +0530

    Initial commit

---
+++ src/main/java/com/data/controller/ServerEventController.java
@@ -0,0 +1,34 @@
+package com.data.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/server-events")
+public class ServerEventController {
+
+    @GetMapping( produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> streamEvents() throws IOException {
+
+        Stream<String> lines =Files.lines(Path.of("src/main/resources/data.txt"));
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+        return Flux.fromStream(lines).filter(linbe-> !linbe.isEmpty())
+                .map(line -> ServerSentEvent.builder(line)
+                        .id(String.valueOf(atomicInteger.getAndIncrement()))
+                        .event("data")
+                        .build())
+                .delayElements(Duration.ofSeconds(1));
+    }
+}
---
+++ src/test/java/com/data/server_sent_event/ServerSentEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.server_sent_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
